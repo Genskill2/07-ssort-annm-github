328f8cc08a75f7190392fbf98f980eff1ddb31e4
annm-github <71374618+annm-github@users.noreply.github.com>
2021-06-30 23:57:05 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,19 +1,20 @@
 void swap_max(int arr[],int l,int n)
-{      int a;
+    {    
+       int a;
        int largest=arr[0];
         int i;
       for (i = l; i <= n; i++) 
            {
              if(largest<arr[i])
                  largest=arr[i];
-                }
-                     a = arr[i];
+            }
+                    a = arr[i];
                     arr[i] = arr[l];
                     arr[l] = a;
-}
+      }
 void ssort(int arr[],int n)
 {
-  for(int i=0;i<n-1;i++)
+  for(int i=0;i<=n-1;i++)
      {
        swap_max(arr,i,n);
      }
